d15d943d4044194760387337ad3be9decb534742
Lê Trung Hiếu <warmdevofficial@gmail.com>
2026-09-08 13:09:37 +0700

    feat(db): add GetUserByEmail and CheckEmailExists sqlc

--- internal/db/queries/user.sql
+++ internal/db/queries/user.sql
@@ -4,3 +4,21 @@ INSERT INTO users (username, email, hashed_password, display_name, birth_day, ge
 RETURNING
     *;
 
+-- name: GetUserByEmail :one
+SELECT
+    *
+FROM
+    users
+WHERE
+    email = $1;
+
+-- name: CheckEmailExists :one
+SELECT
+    EXISTS (
+        SELECT
+            1
+        FROM
+            users
+        WHERE
+            email = $1);
+
