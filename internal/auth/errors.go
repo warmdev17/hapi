50dc38473287e98be4dab097405e752bec4dce44
Lê Trung Hiếu <warmdevofficial@gmail.com>
2026-09-12 14:47:37 +0700

    feat: normalization ddd project structure

--- internal/auth/errors.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "errors"
-
-var (
-	ErrEmailTaken = errors.New("email already registered")
-	ErrInternal   = errors.New("an unexpected error occured while processing your request")
-)
